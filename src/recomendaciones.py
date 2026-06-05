1e48d61aadab5bdf0bc4fab9e277010da9156f3f
tleonard996 <tleonard@udesa.edu.ar>
2026-06-05 15:11:00 -0300

    Update recomendaciones.py

--- src/recomendaciones.py
+++ src/recomendaciones.py
@@ -7,27 +7,39 @@ Created on Fri Jun  5 12:12:42 2026
 
 def buscar_sesiones_similares(
         df,
-        viento
+        ubicacion,
+        viento,
+        direccion
 ):
 
     similares = df[
-        abs(
-            df["Vel. Viento (kn)"]
-            - viento
-        ) <= 3
-    ]
+        (df["Ubicación"] == ubicacion)
+        &
+        (df["Dir. Viento"] == direccion)
+        &
+        (
+            abs(
+                df["Vel. Viento (kn)"]
+                - viento
+            ) <= 3
+        )
+        
 
     return similares
 
 
 def recomendar_wing(
         df,
-        viento
+        ubicacion,
+        viento,
+        direccion
 ):
 
     similares = buscar_sesiones_similares(
         df,
-        viento
+        ubicacion,
+        viento,
+        direccion
     )
 
     if len(similares) == 0:
@@ -38,25 +50,39 @@ def recomendar_wing(
 
         return
 
-    wing = (
-        similares["Wing"]
-        .mode()[0]
+    if len(similares) < 3:
+
+        print(
+            f"Advertencia: solo se encontraron {len(similares)} sesiones similares."
+        )
+
+    promedio_por_wing = (
+        similares
+        .groupby("Wing")["Sensación"]
+        .mean()
     )
 
-    print("\nWing recomendado:")
+    wing_recomendado = (
+        promedio_por_wing.idxmax()
+    )
 
-    print(wing)
+    print("\n===== RECOMENDACIÓN =====")
+
+    print(
+        "Wing recomendado:",
+        wing_recomendado
+    )
+
+    print(
+        "Sensación promedio esperada:",
+        round(
+            promedio_por_wing.max(),
+            2
+        )
+    )
 
     print(
         "Basado en",
         len(similares),
         "sesiones similares"
-    )
-
-    print(
-        "Sensación promedio:",
-        round(
-            similares["Sensación"].mean(),
-            2
-        )
     )

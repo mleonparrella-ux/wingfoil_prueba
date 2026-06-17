8159e96cd956872cc1e4aedd4bf508d9a54a106d
agusfrey <afrey@udesa.edu.ar>
2026-06-17 15:17:43 -0300

    Update analisis_datos.py

--- src/analisis_datos.py
+++ src/analisis_datos.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun  5 12:12:42 2026
-
-@author: usuario
-"""
 
 def mostrar_metricas(df):
 
@@ -45,6 +39,24 @@ def mostrar_metricas(df):
         "Duración promedio:",
         round(
             df["Duración (min)"].mean(), 
+            2
+        )
+    )
+
+
+def mostrar_resumen(df):
+
+    print("\n===== RESUMEN =====")
+
+    print(
+        "Total de sesiones:",
+        len(df)
+    )
+
+    print(
+        "Sensación promedio:",
+        round(
+            df["Sensación"].mean(),
             2
         )
     )
